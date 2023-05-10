3cc7b087652c2d3cb91e53b4acea0f17db09cf58
SergeyKaraman <karamans7997@gmail.com>
2023-05-10 15:01:26 +0300

    Урок 2. Почему вы не можете не использовать API
    Урок 2. Почему вы не можете не использовать API

--- HW1.java
+++ HW1.java
@@ -12,7 +12,6 @@ public class HW1 {
     private static final Logger LOGGER = Logger.getLogger(HW1.class.getName());
 
     public static void main(String[] args) {
-        // Создание строки с данными в формате JSON
         String jsonString = "[{ \"фамилия\":\"Иванов\",\"оценка\":\"5\",\"предмет\":\"Математика\"},{\"фамилия\":\"Петрова\",\"оценка\":\"4\",\"предмет\":\"Информатика\"},{\"фамилия\":\"Краснов\",\"оценка\":\"5\",\"предмет\":\"Физика\"}]";
         String[] students = jsonString.replace("[{", "").replace("}]", "").split("\\},\\{");
 
@@ -22,18 +21,14 @@ public class HW1 {
             String surname = values[0].substring(values[0].indexOf(":") + 2, values[0].length() - 1);
             String grade = values[1].substring(values[1].indexOf(":") + 2);
             String subject = values[2].substring(values[2].indexOf(":") + 2, values[2].length() - 1);
-
             sb.append("Студент ").append(surname).append(" получил ").append(grade).append(" по предмету ")
                     .append(subject).append(".\n");
         }
-
-        // Запись результата в файл
         try (BufferedWriter writer = new BufferedWriter(
                 new OutputStreamWriter(new FileOutputStream("output.txt"), StandardCharsets.UTF_8))) {
             writer.write(sb.toString());
         } catch (IOException e) {
             LOGGER.log(Level.SEVERE, "Ошибка записи в файл", e);
-            // Запись ошибки в лог-файл
             try {
                 FileHandler fileHandler = new FileHandler("log.txt");
                 LOGGER.addHandler(fileHandler);
